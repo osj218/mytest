73a47d0bc7b78868f1e1d75c9302eaee35a007aa
osj21 <osj21@OSJNOT>
2019-01-07 17:14:09 +0900

    person/personTest
    뭘라....

--- mytest/src/com/herbmall/test/Person.java
+++ mytest/src/com/herbmall/test/Person.java
@@ -1,21 +1,21 @@
 package com.herbmall.test;
 
 public class Person {
-	//싱글톤 패턴 - 인스턴스를 하나만 생성하는 디자인 패턴
-	/* Person 클래스를 메모리에 한 번만 올리게 막는다 
-	 * [1] 생성자를 private로 => new연산자를 사용하여, 객체 생성 불가
-	 * [2] get 메서드 만들기 - =static 객체를 만든다
-	 * [3] 멤버변수로 Person을 담을 수 있는 instance 변수를 만든다
-	 *  - static으로 지정해서 한 개만 만들어지게 한다*/
+	//�떛湲��넠 �뙣�꽩 - �씤�뒪�꽩�뒪瑜� �븯�굹留� �깮�꽦�븯�뒗 �뵒�옄�씤 �뙣�꽩
+	/* Person �겢�옒�뒪瑜� 硫붾え由ъ뿉 �븳 踰덈쭔 �삱由ш쾶 留됰뒗�떎 
+	 * [1] �깮�꽦�옄瑜� private濡� => new�뿰�궛�옄瑜� �궗�슜�븯�뿬, 媛앹껜 �깮�꽦 遺덇�
+	 * [2] get 硫붿꽌�뱶 留뚮뱾湲� - =static 媛앹껜瑜� 留뚮뱺�떎
+	 * [3] 硫ㅻ쾭蹂��닔濡� Person�쓣 �떞�쓣 �닔 �엳�뒗 instance 蹂��닔瑜� 留뚮뱺�떎
+	 *  - static�쑝濡� 吏��젙�빐�꽌 �븳 媛쒕쭔 留뚮뱾�뼱吏�寃� �븳�떎*/
 	
-	private static Person instance; // static 멤버변수
-	//=> 클래스 차원에서 하나만 만들어지고, 모든 객체가 공유함
+	private static Person instance; // static 硫ㅻ쾭蹂��닔
+	//=> �겢�옒�뒪 李⑥썝�뿉�꽌 �븯�굹留� 留뚮뱾�뼱吏�怨�, 紐⑤뱺 媛앹껜媛� 怨듭쑀�븿
 	
-	//private 생성자
+	//private �깮�꽦�옄
 	private Person() {
 	}
 	
-	//person 객체를 리턴하는 static 메서드
+	//person 媛앹껜瑜� 由ы꽩�븯�뒗 static 硫붿꽌�뱶
 	public static Person getInstance() {
 		if(instance==null) {
 			instance = new Person();
@@ -23,6 +23,6 @@ public class Person {
 		return instance;
 	}
 	public void showInf() {
-		System.out.println("person클래스의 showinfo 메서드");
+		System.out.println("person�겢�옒�뒪�쓽 showinfo 硫붿꽌�뱶sdgsdgsddg");
 	}
 }
